e7dad5f2d015f5845b41d86d0e9d272b4eba6064
Raymond Chung <raymond_chung2@hotmail.com>
2016-03-06 07:10:33 -0500

    This is my second solo hack, where I am about 65% done within 6 hours out of 14 hours that remained when my team members gave up

---
+++ HanzelBE/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Raymond Chung on 2016-03-05.
+//  Copyright © 2016 Raymond Chung. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    @IBOutlet weak var MapView: MKMapView!
+    let LocationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.LocationManager.delegate = self
+        self.LocationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.LocationManager.requestWhenInUseAuthorization()
+        self.LocationManager.startUpdatingLocation()
+        self.MapView.showsUserLocation = true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let location = locations.last
+        
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        
+        self.MapView.setRegion(region, animated: true)
+        
+        self.LocationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError)
+    {
+        print("Errors: " + error.localizedDescription)
+    }
+    
+    
+}
+
